f1b4b82635a782545bb737a9a8bf46958a4995f3
selo <ahmadseloabadi@gmail.com>
2025-01-22 21:48:43 +0700

    day_1

---
+++ BasicPHP/TypeData.php
@@ -0,0 +1,20 @@
+<?php 
+echo "decimal : ";
+var_dump(1234);
+echo "octal : ";
+var_dump(0123);
+echo "hexadecimal :";
+var_dump(0x1A);
+echo "binary : ";
+var_dump(0b111111);
+echo "underscore di number : ";
+var_dump(1_234_567);
+echo "floating point : ";
+var_dump(1.234);
+echo "floating point dengan e notation plus (1.2x1000): ";
+var_dump(1.2e3);
+echo "floating point dengan e notation minus (7x00.1) : ";
+var_dump(7e-3);
+echo "underscore di floating point : ";
+var_dump(1_123.234);
+?>
