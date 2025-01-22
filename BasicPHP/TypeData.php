944deb0821967ec8c7c61fba85b180dba2a6b88b
selo <ahmadseloabadi@gmail.com>
2025-01-22 22:00:18 +0700

    day_1

--- BasicPHP/TypeData.php
+++ BasicPHP/TypeData.php
@@ -1,4 +1,5 @@
 <?php 
+echo "int\n";
 echo "decimal : ";
 var_dump(1234);
 echo "octal : ";
@@ -9,6 +10,8 @@ echo "binary : ";
 var_dump(0b111111);
 echo "underscore di number : ";
 var_dump(1_234_567);
+
+echo "float \n";
 echo "floating point : ";
 var_dump(1.234);
 echo "floating point dengan e notation plus (1.2x1000): ";
@@ -17,4 +20,30 @@ echo "floating point dengan e notation minus (7x00.1) : ";
 var_dump(7e-3);
 echo "underscore di floating point : ";
 var_dump(1_123.234);
+
+echo "boolean \n";
+echo "true :";
+var_dump(true);
+echo "false :";
+var_dump(false);
+
+echo "string \n";
+echo "nama :";
+echo "selo\n";
+echo 'ini menggunakan single quote';
+echo "\nini\tpake\tdouble\tquote\tstring";
+echo "\nmultiline string\n";
+
+echo <<<BEBAS
+ini contoh penggunaan string 
+menggunakan,heredoc untuk string 
+yang panjang jadi simple\n
+BEBAS; 
+echo <<<'BEBAS'
+ini contoh penggunaan string 
+menggunakan,nowdoc untuk string 
+yang panjang jadi simple\n
+BEBAS; 
+
+
 ?>
