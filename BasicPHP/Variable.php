c69f716522214f80e5e6442e5b6d2501e672fba2
selo <ahmadseloabadi@gmail.com>
2025-01-22 22:55:54 +0700

    null

--- BasicPHP/Variable.php
+++ BasicPHP/Variable.php
@@ -1,6 +1,6 @@
 <?php 
 
-echo "variable\n";
+echo "--variable---\n";
 // variable pada php berdifat mutable(bisa dirubah)
 $name = "selo";
 $age = 25;
@@ -13,7 +13,7 @@ echo "age :";
 echo $age;
 echo "\n";
 
-echo "variable variables\n";
+echo "---variable variables---\n";
 // note: tidak disarankan karna membuat binggung
 $country="ind";
 $$country = "wonderful";
@@ -23,7 +23,7 @@ echo $country;
 echo "\nvariables:";
 echo $ind;
 
-echo "\n constant variable";
+echo "\n---constant variable---\n";
 // variable imutable pada php hanya dapat dideklarasi sekali saja
 
 define("AUTHOR","ahmad selo abadi");
@@ -34,6 +34,24 @@ echo AUTHOR;
 echo "\n";
 echo "progress :";
 echo PROGRESS;
+
+echo "\n---data null---\n";
+
+$data_null = null;
+$contoh = "contoh";
+
+echo "contoh data null :";
+echo $data_null;
+echo "\n";
+
+echo 'cek variable $data_null null :';
+echo is_null($data_null);
+echo "\n";
+echo 'cek variable $contoh null :';
+echo var_dump(is_null($contoh));
+echo "\n";
+// echo "cek null pada variable yang tidak di deklarasi :";
+// echo is_null($tidakada);
 
 
 ?>
