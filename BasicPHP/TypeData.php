eae26fe7d530794c552392af0e54c05fc988ff2a
selo <ahmadseloabadi@gmail.com>
2025-01-23 00:02:44 +0700

    docs : add array

--- BasicPHP/TypeData.php
+++ BasicPHP/TypeData.php
@@ -1,5 +1,5 @@
 <?php 
-echo "int\n";
+echo "----int---\n";
 echo "decimal : ";
 var_dump(1234);
 echo "octal : ";
@@ -11,7 +11,7 @@ var_dump(0b111111);
 echo "underscore di number : ";
 var_dump(1_234_567);
 
-echo "float \n";
+echo "-----float----- \n";
 echo "floating point : ";
 var_dump(1.234);
 echo "floating point dengan e notation plus (1.2x1000): ";
@@ -21,13 +21,13 @@ var_dump(7e-3);
 echo "underscore di floating point : ";
 var_dump(1_123.234);
 
-echo "boolean \n";
+echo "----boolean---- \n";
 echo "true :";
 var_dump(true);
 echo "false :";
 var_dump(false);
 
-echo "string \n";
+echo "----string----- \n";
 echo "nama :";
 echo "selo\n";
 echo 'ini menggunakan single quote';
@@ -44,6 +44,13 @@ ini contoh penggunaan string
 menggunakan,nowdoc untuk string 
 yang panjang jadi simple\n
 BEBAS; 
+
+echo "\n----array----- \n";
+$array = array(1,2,3,4);
+
+echo "array :";
+print_r($array);
+
 
 
 ?>
