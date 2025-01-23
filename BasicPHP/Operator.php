0e6f5d084067a9592632c8f9b08a3782f90968c2
selo <ahmadseloabadi@gmail.com>
2025-01-23 16:47:46 +0700

    docs : perbaikan penulisan

--- BasicPHP/Operator.php
+++ BasicPHP/Operator.php
@@ -3,22 +3,22 @@
 $a = 10;
 $b = 3;
 echo "\n\n---operator aritmatika---\n\n";
-echo "initialisasi nilai a : ".$a .", nilai b : ".$b;
-echo "\npenjumlahan :".$a+$b;
-echo "\npengurangan :".$a-$b;
-echo "\nperkalian :".$a*$b;
-echo "\npembagian :".$a/$b;
-echo "\nmod :".$a%$b;
-echo "\npangkat :".$a**$b;
+echo "initialisasi nilai a : ",$a ,", nilai b : ",$b;
+echo "\npenjumlahan :",$a+$b;
+echo "\npengurangan :",$a-$b;
+echo "\nperkalian :",$a*$b;
+echo "\npembagian :",$a/$b;
+echo "\nmod :",$a%$b;
+echo "\npangkat :",$a**$b;
 
 echo "\n\n---operator penugasan aritmatika---\n\n";
 $total = 0;
-echo "initialisasi nilai total : ".$total.", nilai b : ".$a;
-echo "\npenugasan penjumlahan :".$total+=$a;
-echo "\npenugasan perkalian :".$total*=$a;
-echo "\npenugasan pengurangan :".$total-=$a;
-echo "\npenugasan pembagian :".$total/=$a;
-echo "\npenugasan mod :".$total%=$a;
+echo "initialisasi nilai total : ",$total,", nilai b : ",$a;
+echo "\npenugasan penjumlahan :",$total+=$a;
+echo "\npenugasan perkalian :",$total*=$a;
+echo "\npenugasan pengurangan :",$total-=$a;
+echo "\npenugasan pembagian :",$total/=$a;
+echo "\npenugasan mod :",$total%=$a;
 
 echo "\n\n---operator perbandingan---\n\n";
 
@@ -37,7 +37,7 @@ if ($a == $b) {
     echo "Nilai \$a = 10 tidak sama dengan \$b = '10' (==)";
 }
 
-// note : $a == $b akan bernilai true karena PHP hanya membandingkan nilainya saja, tanpa memperhatikan tipe datanya.
+// note : $a == $b akan bernilai true karena PHP hanya membandingkan nilainya saja, tanpa memperhatikan tipe datanya,
 echo "\n---Operator === (Identik)---\n";
 
 if ($a === $b) {
@@ -45,7 +45,7 @@ if ($a === $b) {
 } else {
     echo "Nilai \$a = 10 tidak identik dengan \$b = '10' (===)";
 }
-// note : $a === $b akan bernilai false karena meskipun nilainya sama, tipe datanya berbeda ($a adalah integer dan $b adalah string).
+// note : $a === $b akan bernilai false karena meskipun nilainya sama, tipe datanya berbeda ($a adalah integer dan $b adalah string),
 echo "\n---Operator != (Tidak sama dengan)---\n";
 $c=20;
 echo "nilai\$b = '10' : ";
@@ -82,7 +82,7 @@ if ($a < $c) {
 
 echo "\n---Operator <=> (Spaceship)---\n";
 $result = $a <=> $c;
-// note : Mengembalikan nilai -1 jika nilai di sebelah kiri lebih kecil, 0 jika sama, dan 1 jika lebih besar.
+// note : Mengembalikan nilai -1 jika nilai di sebelah kiri lebih kecil, 0 jika sama, dan 1 jika lebih besar,
 if ($result == -1) {
     echo "\$a = 10 lebih kecil dari \$c = 20 (<=>)";
 } elseif ($result == 0) {
@@ -93,49 +93,49 @@ if ($result == -1) {
 
 echo "\n\n---Operator logika---\n\n";
 
-echo "\n---Operator AND (&& atau and)---\n";//Mengembalikan true jika kedua operand bernilai true.
+echo "\n---Operator AND (&& atau and)---\n";//Mengembalikan true jika kedua operand bernilai true,
 
 var_dump(true && true);  // true
 var_dump(true && false); // false
 var_dump(false && false); // false
 
-echo "\n---Operator OR (|| atau or)---\n"; //Mengembalikan true jika salah satu operand bernilai true.
+echo "\n---Operator OR (|| atau or)---\n"; //Mengembalikan true jika salah satu operand bernilai true,
 var_dump(true || false);  // true
 var_dump(false || false); // false
 var_dump(true || true);   // true
 
-echo "\n---Operator NOT (!)---\n"; //Membalikkan nilai boolean.
+echo "\n---Operator NOT (!)---\n"; //Membalikkan nilai boolean,
 var_dump(!true);  // false
 var_dump(!false); // true
 
-echo "\n---Operator XOR---\n"; //Mengembalikan true jika salah satu operand bernilai true, tetapi tidak keduanya.
+echo "\n---Operator XOR---\n"; //Mengembalikan true jika salah satu operand bernilai true, tetapi tidak keduanya,
 var_dump(true xor false);  // true
 var_dump(true xor true);   // false
 var_dump(false xor false); // false
 
 echo "\n\n---Operator increment dan decrement---\n\n";
 
-echo "\n---Pre-Increment (++\$x)---\n"; //Menambah nilai variabel sebelum variabel digunakan.
 $x = 5;
-echo ++$x; // Output: 6 (nilai $x langsung ditambah 1 sebelum dicetak)
+echo "initialisasi \$x :",$x;
+echo "\n---Pre-Increment (++\$x)---\n"; //Menambah nilai variabel sebelum variabel digunakan,
+echo "implementasi :",++$x; // Output: 6 (nilai $x langsung ditambah 1 sebelum dicetak)
 echo "\n";
-echo $x;   // Output: 6 (nilai $x sudah berubah menjadi 6)
+echo "result :",$x;   // Output: 6 (nilai $x sudah berubah menjadi 6)
 
-echo "\n---Post-Increment (\$x++)---\n"; //Menambah nilai variabel setelah variabel digunakan.
-echo $x++; // Output: 5 (nilai $x dicetak dulu, baru ditambah 1)
+echo "\n---Post-Increment (\$x++)---\n"; //Menambah nilai variabel setelah variabel digunakan,
+echo "implementasi :",$x++; // Output: 5 (nilai $x dicetak dulu, baru ditambah 1)
 echo "\n";
-echo $x;   // Output: 6 (nilai $x sekarang sudah menjadi 6)
+echo "result :",$x;   // Output: 6 (nilai $x sekarang sudah menjadi 6)
 
 echo "\n---Pre-Decrement (--\$x)---\n"; //Pre-Decrement (--$x)
-echo --$x; // Output: 4 (nilai $x langsung dikurangi 1 sebelum dicetak)
+echo "implementasi :",--$x; // Output: 4 (nilai $x langsung dikurangi 1 sebelum dicetak)
 echo "\n";
-echo $x;   // Output: 4 (nilai $x sudah berubah menjadi 4)
+echo "result :",$x;   // Output: 4 (nilai $x sudah berubah menjadi 4)
 
-echo "\n---Pre-Decrement (\$x--)---\n"; //Mengurangi nilai variabel setelah variabel digunakan.
-$x = 5;
-echo $x--; // Output: 5 (nilai $x dicetak dulu, baru dikurangi 1)
+echo "\n---Pre-Decrement (\$x--)---\n"; //Mengurangi nilai variabel setelah variabel digunakan,
+echo "implementasi :",$x--; // Output: 5 (nilai $x dicetak dulu, baru dikurangi 1)
 echo "\n";
-echo $x;   // Output: 4 (nilai $x sekarang sudah menjadi 4)
+echo "result :",$x;   // Output: 4 (nilai $x sekarang sudah menjadi 4)
 
 
 
