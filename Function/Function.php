ad557f338a8f008dda4c38f214149e4bc4bfb4d6
selo <ahmadseloabadi@gmail.com>
2025-01-26 00:32:45 +0700

    docs : function

--- Function/Function.php
+++ Function/Function.php
@@ -17,10 +17,23 @@ function perkalian($a,$b)
 
 perkalian(3,5);
 
-function penjumlahan($x=3,$y=5)
+
+function penjumlahan($x=3,$y=5) // parameter dengan nilia default
 {   
     $hasil= $x+$y;
-    echo "hasil perkalian dari $x dan $y : $hasil". PHP_EOL;
+    echo "hasil penjumlahan dari $x dan $y : $hasil". PHP_EOL;
 }
+
+penjumlahan();
+function pengurangan(int $x,int $y) // parameter dengan type data yang di tentukan
+{   
+    $hasil= $x-$y;
+    echo "hasil pengurangan dari $x dan $y : $hasil". PHP_EOL;
+}
+
+pengurangan(5,3);
+pengurangan("5","1");
+
+
 
 ?>
