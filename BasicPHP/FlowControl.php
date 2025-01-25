7df88005e2e7263f48a64c8460423cb22d2e5160
selo <ahmadseloabadi@gmail.com>
2025-01-25 18:45:16 +0700

    docs : continue learning

--- BasicPHP/FlowControl.php
+++ BasicPHP/FlowControl.php
@@ -145,7 +145,19 @@ $buah = ["Apel", "Jeruk", "Mangga", "Pisang"];
 
 // Menggunakan foreach untuk mencetak setiap elemen array
 foreach ($buah as $item) {
-    echo "Buah: $item". PHP_EOL;
+    echo "Buah : $item". PHP_EOL;
+}
+
+// Array asosiatif
+$mahasiswa = [
+    "nama" => "Budi",
+    "nim" => "123456",
+    "jurusan" => "Teknik Informatika"
+];
+
+// Menggunakan foreach untuk mencetak key dan value
+foreach ($mahasiswa as $key => $value) {
+    echo ucfirst($key) . ": $value<br>";
 }
 
 ?>
