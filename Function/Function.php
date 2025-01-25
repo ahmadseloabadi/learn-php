e7fde1f3ac88591524b6d5dddf7ed67d1186b57d
selo <ahmadseloabadi@gmail.com>
2025-01-25 22:43:09 +0700

    docs : function

--- Function/Function.php
+++ Function/Function.php
@@ -1,10 +1,26 @@
 <?php 
-
+echo "\nbasic function\n";
 function sayHello()
 {
     echo "hello everyone";
 }
 
 sayHello();
+
+echo "\nfunction argument\n";
+
+function perkalian($a,$b)
+{   
+    $result = $a*$b;
+    echo "hasil perkalian dari $a dan $b : $result". PHP_EOL ;
+}
+
+perkalian(3,5);
+
+function penjumlahan($x=3,$y=5)
+{   
+    $hasil= $x+$y;
+    echo "hasil perkalian dari $x dan $y : $hasil". PHP_EOL;
+}
 
 ?>
