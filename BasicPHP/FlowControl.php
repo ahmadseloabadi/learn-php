91e37363bf8e6cffe57ee0ec2519f383dfdd3fd1
selo <ahmadseloabadi@gmail.com>
2025-01-25 15:38:11 +0700

    docs :do  while loop

--- BasicPHP/FlowControl.php
+++ BasicPHP/FlowControl.php
@@ -111,4 +111,15 @@ while ($jumlah <=10){
     echo "-while loop : ke-".$jumlah.PHP_EOL;
     $jumlah++;
 };
+echo "\n---do while loop---\n";
+
+$jum = 10;
+do{
+    echo "-do while loop : ke-".$jum.PHP_EOL;
+    $jum++;
+}while($jum<=20);
+// note :
+// do: Blok kode di dalam do akan dijalankan terlebih dahulu, sebelum memeriksa kondisi.
+// while ($jum<=20): Kondisi diperiksa setelah blok kode dijalankan. Jika kondisi benar, perulangan berlanjut.
+
 ?>
