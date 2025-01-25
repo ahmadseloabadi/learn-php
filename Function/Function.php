b88af21ac8a19dc10b8a2829eae640228c9c10d2
selo <ahmadseloabadi@gmail.com>
2025-01-26 01:24:19 +0700

    docs : variable function

--- Function/Function.php
+++ Function/Function.php
@@ -112,4 +112,21 @@ function division(int $a , int $b): float
 $divi = division(2,4);
 echo "hasil pembagian : $divi". PHP_EOL;
 
+echo "--variable function---". PHP_EOL;
+$variablefunction = "penjumlahan";
+$variablefunction(6,7);
+
+function helloBro(string $name, $filter)
+{
+    $finalname = $filter($name);
+    echo "Hello $finalname". PHP_EOL;
+}
+
+function sampleFunction(string $name):string
+{
+    return "ini dari sample function $name";
+}
+helloBro("Sungep","strtoupper");// strtoupper sendiri merupakan fungsi dari php untuk membuat huruf menjadi kapital 
+helloBro("Sungep","sampleFunction"); 
+
 ?>
