059d7368c8fb55f7b54182a687a80c9e9c8e8d73
selo <ahmadseloabadi@gmail.com>
2025-01-28 01:29:01 +0700

    docs : include_once and require_once

--- Function/IncludeAndRequire.php
+++ Function/IncludeAndRequire.php
@@ -4,19 +4,20 @@
 echo "### Contoh Penggunaan require dan include ###\n\n";
 
 // Menggunakan include untuk menyisipkan MyFunction.php
-// echo "----Menggunakan include:\n";
-// include 'lib/MyFunction.php';//contoh benar
-// include 'MyFunction.php';//contoh salah
-// echo "----Bagian ini tetap dijalankan meskipun include gagal.\n\n";
-// echo sayHelloTo();
+echo "----Menggunakan include:\n";
+include 'lib/MyFunction.php';//contoh benar
+include 'MyFunction.php';//contoh salah
+echo "----Bagian ini tetap dijalankan meskipun include gagal.\n\n";
+echo sayHelloTo();
 
 // Menggunakan require untuk menyisipkan MyFunction.php
 echo "---Menggunakan require:\n";
 require 'lib/MyFunction.php';//contoh benar
-// require 'MyFunction.php';//contoh salah
+require 'MyFunction.php';//contoh salah
 echo "----Bagian ini tetap dijalankan jika require berhasil.\n\n";
 echo sayHelloTo();
-### Contoh Penggunaan require dan include ###
+
+### penjelasan require dan include ###
 
 // Menggunakan include:
 // Mengimpor file lain ke dalam skrip.
@@ -28,5 +29,38 @@ echo sayHelloTo();
 // Cocok untuk file yang sangat penting, seperti file konfigurasi atau dependensi utama.
 
 
+// Contoh penggunaan require_once dan include_once
+
+echo "### Contoh Penggunaan require_once dan include_once ###\n\n";
+
+// Menggunakan include_once untuk menyisipkan MyFunction.php
+// echo "----Menggunakan include_once:\n";
+// include_once 'lib/MyFunction.php';//contoh benar
+// include_once 'MyFunction.php';//contoh salah
+// echo "----Bagian ini tetap dijalankan meskipun include_once gagal.\n\n";
+// echo sayHelloTo();
+
+// Menggunakan require_once untuk menyisipkan MyFunction.php
+// echo "---Menggunakan require_once:\n";
+// require_once 'lib/MyFunction.php';//contoh benar
+// require_once 'MyFunction.php';//contoh salah
+// echo "----Bagian ini tetap dijalankan jika require_once berhasil.\n\n";
+// echo sayHelloTo();
+
+### penjelasan require_once dan include_once ###
+
+// Menggunakan include_once:
+// Mengimpor file hanya sekali, meskipun dipanggil lebih dari satu kali.
+// Jika file yang ingin di-include sudah dimasukkan sebelumnya, maka tidak akan dimasukkan lagi.
+// Cocok untuk file yang ingin dipastikan hanya dimasukkan sekali, seperti file konfigurasi atau library.
+
+// Menggunakan require_once:
+// Sama seperti include_once, tetapi jika file tidak ditemukan, akan menyebabkan fatal error dan menghentikan eksekusi skrip.
+// Ini digunakan untuk file yang sangat penting dan harus ada di dalam aplikasi.
+
+### alasan penggunaan require_once dan include_once ###
+// require dan include selalu memuat file yang diinginkan, meskipun digunakan berulang kali.
+// Jika file yang sama diambil berulang kali, ini dapat menyebabkan masalah, seperti error redeclare function karena definisi fungsi yang duplikat.
+// require_once dan include_once di PHP berguna untuk memastikan file yang sama hanya dimuat sekali, sehingga mencegah error tersebut.
 
 ?>
