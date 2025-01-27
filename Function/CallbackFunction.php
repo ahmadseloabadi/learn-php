b81320edab4255c3d72f91555915310a7c0a74ab
selo <ahmadseloabadi@gmail.com>
2025-01-27 23:34:01 +0700

    docs : callback function

--- Function/CallbackFunction.php
+++ Function/CallbackFunction.php
@@ -10,4 +10,13 @@ function helloBro(string $name, callable $filter)
 helloBro("callback x anonymous function", function ($name) { return strtoupper($name);});
 helloBro("CALLBACK X ARROW FUNCTION",fn($name) => strtolower($name));
 helloBro("callback x variable function","strtoupper");
+
+function sayHello(string $name, callable $filter)
+{
+    $finalname = $filter($name); //tanpa call_user_func
+    echo "sayHello $finalname". PHP_EOL;
+};
+sayHello("callback x anonymous function", function ($name) { return strtoupper($name);});
+sayHello("CALLBACK X ARROW FUNCTION",fn($name) => strtolower($name));
+sayHello("callback x variable function","strtoupper");
 ?>
